63054de29626b42bdaadbc7475cbc3c50632656c
Justin <69047638+Justin-Meme-Sometimes@users.noreply.github.com>
2026-04-14 23:40:44 -0400

    Update rplidar_a1.lua
    made this better aswell

--- src/hamerschlag_haul/config/rplidar_a1.lua
+++ src/hamerschlag_haul/config/rplidar_a1.lua
@@ -39,16 +39,19 @@ TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.2
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 10.
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight = 1e4
-TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.2)
-TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 10
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 400
+TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(1.0)
+TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.1
+TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 3
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 25
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 100
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 1000
 
-POSE_GRAPH.optimize_every_n_nodes = 20
+POSE_GRAPH.optimize_every_n_nodes = 5
 POSE_GRAPH.optimization_problem.ceres_solver_options.num_threads = 2
-POSE_GRAPH.constraint_builder.min_score = 0.55
-POSE_GRAPH.constraint_builder.global_localization_min_score = 0.60
+POSE_GRAPH.constraint_builder.min_score = 0.50
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.55
 POSE_GRAPH.constraint_builder.max_constraint_distance = 15.0
+POSE_GRAPH.constraint_builder.sampling_ratio = 0.5
 POSE_GRAPH.optimization_problem.huber_scale = 1e2
 
 return options
